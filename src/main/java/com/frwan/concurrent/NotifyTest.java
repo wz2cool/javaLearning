0468016f840e0bcbcf84e9bf93175837fc7def78
wz2cool <wz2cool@live.cn>
2018-04-24 14:44:32 +0800

    add

--- src/main/java/com/frwan/concurrent/NotifyTest.java
+++ src/main/java/com/frwan/concurrent/NotifyTest.java
@@ -2,8 +2,35 @@ package com.frwan.concurrent;
 
 public class NotifyTest {
 
-    public static void testNoitfy() {
+    /**
+     * 1） wait 释放monitor 对象所有有权
+     * 2） 如果wait 被唤起（notify/notifyAll）要去执行，一定是竞争到了monitor，
+     * notify 只会唤醒第一个线程， notify唤起所有线程。
+     *
+     * @throws InterruptedException
+     */
+    public static void testNotify() throws InterruptedException {
+        final NotifyTest test = new NotifyTest();
 
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    test.testMethod();
+                }
+            }).start();
+        }
+
+        Thread.sleep(1000);
+        synchronized (test) {
+            test.notify();
+        }
+
+        Thread.sleep(3000);
+        System.out.println("-----------分割线-------------");
+
+        synchronized (test) {
+            test.notifyAll();
+        }
     }
 
     private synchronized void testMethod() {
--- src/main/java/com/frwan/main.java
+++ src/main/java/com/frwan/main.java
@@ -1,9 +1,9 @@
 package com.frwan;
 
-import com.frwan.concurrent.WaitTest;
+import com.frwan.concurrent.NotifyTest;
 
 public class main {
-    public static void main(String[] args) {
-        WaitTest.testWait();
+    public static void main(String[] args) throws Exception {
+        NotifyTest.testNotify();
     }
 }
